7760e2416e9ee52ecdd8750b2004a2dedb3d3173
nematode195 <mike.tallsmall@gmail.com>
2016-09-08 11:24:09 -0400

    wire up the driver

--- driver.js
+++ driver.js
@@ -1,8 +1,10 @@
 
 
-var tDAO = require("/tsdao");
-var domain = require("domain");
+var tDAO = require("./tsdao");
+//var domain = require("./domain");
 
 var mytDAO = tDAO.create();
 
 var testMsg = mytDAO.test();
+
+console.log("test message" + testMsg);
